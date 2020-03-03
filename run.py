a316b6f8ac06cfbc580e8d3534dcbe16bc65549a
Pedro Bibiloni <p.bibiloni@uib.es>
2020-03-03 17:03:29 +0100

    Añade solución

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ def load_dcm(filename):
 
 
 def compute_snr(signal_power, noise_power):
-    pass
+    return np.sqrt(signal_power/noise_power)
 
 
 def compute_cnr(signal_contrast, noise_power):
-    pass
+    return signal_contrast/np.sqrt(noise_power)
 
 
 def main():
@@ -30,6 +30,46 @@ def main():
     plt.subplot(121), plt.imshow(img_rest, cmap=plt.cm.bone)
     plt.subplot(122), plt.plot(histogram), plt.xticks(np.arange(0, 2**8+1, step=2**6), np.arange(0, 2**16+1, step=2**14), rotation=45)
     plt.show()
+
+    # Convertir imagen a float para evitar errores por overflow.
+    img_rest = img_rest.astype('float64')
+
+    # Medidas de calidad de imagen
+    noise_threshold = 300   # Medido en [T1]
+    noise_mask = (img_rest < noise_threshold) * (img_rest > 0)
+    noise_power = np.average(np.square(img_rest[noise_mask]))
+
+    signal_mask = img_rest > noise_threshold
+    signal_power = np.average(np.square(img_rest[signal_mask]))
+    signal_contrast = np.max(img_rest[signal_mask] - np.min(img_rest[signal_mask]))
+
+    assert( np.all(img_rest[noise_mask] > 0))
+
+    print(f'# Medidas de calidad de la imagen')
+    print(f'Potencia de la señal: {signal_power} [T1^2].')
+    print(f'Contraste de la señal: {signal_contrast} [T1].')
+    print(f'Potencia del ruido: {noise_power} [T1^2].')
+    print(f'SNR: {compute_snr(signal_power, noise_power)} [1/1].')
+    print(f'CNR: {compute_cnr(signal_contrast, noise_power)} [1/1].')
+
+    plt.subplot(221), plt.imshow(signal_mask)
+    plt.subplot(222), plt.imshow(noise_mask)
+    plt.subplot(223), plt.imshow(img_rest * signal_mask, cmap=plt.cm.bone)
+    plt.subplot(224), plt.imshow(img_rest * noise_mask, cmap=plt.cm.bone)
+    plt.show()
+
+    # Movimiento
+
+    movement_thr = 3e3
+    movement_img = dcms[0].pixel_array - dcms[1].pixel_array
+    movement_mask = np.abs(movement_img) > movement_thr
+
+    plt.subplot(221), plt.imshow(dcms[0].pixel_array, cmap=plt.cm.bone)
+    plt.subplot(222), plt.imshow(dcms[1].pixel_array, cmap=plt.cm.bone)
+    plt.subplot(223), plt.imshow(movement_img, cmap=plt.cm.bone)
+    plt.subplot(224), plt.imshow(movement_mask)
+    plt.show()
+
 
 
 if __name__ == '__main__':
